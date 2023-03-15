6c0ca4886dd879be559afc117e0a5f7459d5681c
Youssef Girgeis <youssef_girgeis@hotmail.com>
2023-03-14 23:05:40 -0400

    create the header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,52 @@
+import styles from './Header.module.scss';
+import { Link } from 'react-router-dom';
+import { FaShoppingCart } from 'react-icons/fa';
 
+const logo = (
+  <div className={styles.logo}>
+    <Link to='/'>
+      <h2>
+        e<span>Shop</span>.
+      </h2>
+    </Link>
+  </div>
+);
+
+const cart = (
+  <span className={styles.cart}>
+    <Link to='/cart'>
+      Cart <FaShoppingCart size={20} />
+      <p>0</p>
+    </Link>
+  </span>
+);
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className={styles.header}>
+        {logo}
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact Us</Link>
+            </li>
+          </ul>
+          <div className={styles['header-right']}>
+            <span className={styles.links}>
+              <Link to='/login'>Login</Link>
+              <Link to='/register'>Register</Link>
+              <Link to='/order-history'>My Orders</Link>
+            </span>
+            {cart}
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
