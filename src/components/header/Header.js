cd4d24819bf5585ed8dd708d45cc6e97aabf5c36
Youssef Girgeis <youssef_girgeis@hotmail.com>
2023-03-15 22:33:29 -0400

    make menu responsive

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,8 @@
 import styles from './Header.module.scss';
 import { Link } from 'react-router-dom';
-import { FaShoppingCart } from 'react-icons/fa';
+import { FaShoppingCart, FaTimes } from 'react-icons/fa';
+import { HiOutlineMenuAlt3 } from 'react-icons/hi';
+import { useState } from 'react';
 
 const logo = (
   <div className={styles.logo}>
@@ -22,12 +24,38 @@ const cart = (
 );
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const hideMenu = () => {
+    setShowMenu(false);
+  };
+
   return (
     <header>
       <div className={styles.header}>
         {logo}
-        <nav>
-          <ul>
+        <nav
+          className={
+            showMenu ? `${styles['show-nav']}` : `${styles['hide-nav']}`
+          }
+        >
+          <div
+            className={
+              showMenu
+                ? `${styles['nav-wrapper']} ${styles['show-nav-wrapper']}`
+                : `${styles['nav-wrapper']}`
+            }
+            onClick={hideMenu}
+          ></div>
+          <ul onClick={hideMenu}>
+            <li className={styles['logo-mobile']}>
+              {logo}
+              <FaTimes size={22} color='#fff' onClick={hideMenu} />
+            </li>
             <li>
               <Link to='/'>Home</Link>
             </li>
@@ -35,7 +63,7 @@ const Header = () => {
               <Link to='/contact'>Contact Us</Link>
             </li>
           </ul>
-          <div className={styles['header-right']}>
+          <div className={styles['header-right']} onClick={hideMenu}>
             <span className={styles.links}>
               <Link to='/login'>Login</Link>
               <Link to='/register'>Register</Link>
@@ -44,6 +72,11 @@ const Header = () => {
             {cart}
           </div>
         </nav>
+
+        <div className={styles['menu-icon']}>
+          {cart}
+          <HiOutlineMenuAlt3 size={28} onClick={toggleMenu} />
+        </div>
       </div>
     </header>
   );
